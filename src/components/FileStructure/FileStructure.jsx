cb3625631d889b0404d04dfb87341c96541a7ab3
Trevor VanDoren <vandoren.trevor@gmail.com>
2021-05-11 14:55:22 -0500

    added toggle to expand/collapse folders in the folder tree

--- src/components/FileStructure/FileStructure.jsx
+++ src/components/FileStructure/FileStructure.jsx
@@ -20,13 +20,54 @@ class FileStructure extends Component {
       modified: new Date(),
     };
 
+    this.testFolder1 = {
+      type: "folder",
+      name: "Subfolder",
+      modified: new Date(),
+      size: 0,
+      children: [this.testFile1, this.testFile2],
+    };
+
     this.testFolder = {
       type: "folder",
       name: "Documents",
       modified: new Date(),
-      children: [this.testFile1, this.testFile2],
+      children: [this.testFolder1, this.testFile1, this.testFile2],
     };
+
+    this.state = { test: true };
   }
+
+  // displayFileTree = (rootFolder) => {
+  //   let isExpanded = false;
+  //
+  //   let childFolders =
+  //     rootFolder.children &&
+  //     rootFolder.children.map((node) => {
+  //       return this.displayFileTree(node);
+  //     });
+  //
+  //   return (
+  //     <div className={styles.file_grid}>
+  //       <div
+  //         className={styles.arrow}
+  //         onClick={() => {
+  //           isExpanded = !isExpanded;
+  //           console.log(isExpanded);
+  //         }}
+  //       >
+  //         {rootFolder.children ? "V" : ""}
+  //       </div>
+  //       <div className={styles.docname}>{rootFolder.name}</div>
+  //       <div
+  //         className={styles.doclist}
+  //         style={{ display: isExpanded ? "" : "none" }}
+  //       >
+  //         {childFolders}
+  //       </div>
+  //     </div>
+  //   );
+  // }
 
   displaySelectedFolder = (selectedFolder) => {
     // if the selected folder doesn't have content, don't display anything
@@ -41,7 +82,9 @@ class FileStructure extends Component {
           <td>{node.type === "file" ? "FI" : "FO"}</td>
           <td>{node.name}</td>
           <td>{`${node.modified.getMonth()}/${node.modified.getDate()}/${node.modified.getFullYear()}`}</td>
-          <td>{node.type === "folder" ? "" : `${node.size} KB`}</td>
+          <td style={{ textAlign: "right" }}>
+            {node.type === "folder" ? "" : `${node.size} KB`}
+          </td>
         </tr>
       );
     });
@@ -50,7 +93,9 @@ class FileStructure extends Component {
   render() {
     return (
       <div className={styles.grid_container}>
-        <div className={styles.tree_column}></div>
+        <div className={styles.tree_column}>
+          <TreeItem folder={this.testFolder} />
+        </div>
         <div className={styles.list_column}>
           <table>
             <thead>
@@ -58,11 +103,49 @@ class FileStructure extends Component {
                 <th />
                 <th>Name</th>
                 <th>Date Modified</th>
-                <th>File Size</th>
+                <th style={{ textAlign: "right" }}>File Size</th>
               </tr>
             </thead>
             <tbody>{this.displaySelectedFolder(this.testFolder)}</tbody>
           </table>
+        </div>
+      </div>
+    );
+  }
+}
+
+class TreeItem extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      childrenVisible: false,
+    };
+  }
+
+  render() {
+    let childFolders =
+      this.props.folder.children &&
+      this.props.folder.children.map((node, index) => {
+        return <TreeItem folder={node} key={index} />;
+      });
+
+    return (
+      <div className={styles.file_grid}>
+        <div
+          className={styles.arrow}
+          onClick={() => {
+            this.setState({ childrenVisible: !this.state.childrenVisible });
+          }}
+        >
+          {this.props.folder.children ? "V" : ""}
+        </div>
+        <div className={styles.docname}>{this.props.folder.name}</div>
+        <div
+          className={styles.doclist}
+          style={{ display: this.state.childrenVisible ? "block" : "none" }}
+        >
+          {childFolders}
         </div>
       </div>
     );
