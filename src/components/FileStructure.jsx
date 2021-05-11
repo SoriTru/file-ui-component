69b0a633fa87602ae422e0547fe42a522c79feed
Trevor VanDoren <vandoren.trevor@gmail.com>
2021-05-11 11:05:42 -0500

    set up the project using react-create-app

---
+++ src/components/FileStructure.jsx
@@ -0,0 +1,9 @@
+import React, {Component} from "react";
+
+class FileStructure extends Component {
+    render() {
+        return <div>Stuff's gonna go here</div>
+    }
+}
+
+export default FileStructure
